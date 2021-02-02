0277f23a15caf8799d67699039b1de8d839701b9
Ricardo Melo Joia <ricardo.joia@gmail.com>
2021-02-02 16:21:31 +0000

    fixing cypress

--- AwesomeProduct/ClientApp/cypress/integration/home/home.component.spec.ts
+++ AwesomeProduct/ClientApp/cypress/integration/home/home.component.spec.ts
@@ -1,25 +1,29 @@
-beforeEach(() => {
-  cy.visit('/');
-});
+describe('home component', () => {
 
-it('should have correct options on navigation bar', () => {
-  cy.get('a.nav-link').eq(0)
-    .should('contain.text', 'Home')
-    .should('have.attr', 'href', '/');
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
-  cy.get('a.nav-link').eq(1)
-    .should('contain.text', 'Process')
-    .should('have.attr', 'href', '/batch-process');
+  it('should have correct options on navigation bar', () => {
+    cy.get('a.nav-link').eq(0)
+      .should('contain.text', 'Home')
+      .should('have.attr', 'href', '/');
 
-  cy.get('a.nav-link').eq(2)
-    .should('contain.text', 'History')
-    .should('have.attr', 'href', '/history');
-});
+    cy.get('a.nav-link').eq(1)
+      .should('contain.text', 'Process')
+      .should('have.attr', 'href', '/batch-process');
 
-it('should contain correct Brand', () => {
-  cy.get('a.navbar-brand').should('contain.text', 'AwesomeProduct');
-});
+    cy.get('a.nav-link').eq(2)
+      .should('contain.text', 'History')
+      .should('have.attr', 'href', '/history');
+  });
 
-it('should contain correct Header', () => {
-  cy.get('app-home').get('h1').should('contain.text', 'Hello Awesome user!');
-});
+  it('should contain correct Brand', () => {
+    cy.get('a.navbar-brand').should('contain.text', 'AwesomeProduct');
+  });
+
+  it('should contain correct Header', () => {
+    cy.get('app-home').get('h1').should('contain.text', 'Hello Awesome user!');
+  });
+
+})
